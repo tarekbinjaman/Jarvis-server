1125f0e30decf2706b1b455799103db67ba63195
tarekbinjaman <tarekbinjaman@gmail.com>
2026-03-03 08:16:12 +0600

    done

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const {getResponse} = require("./brain")
 const app = express();
 app.use(cors(
     {
-    origin: ["https://tarek-jarvis-client.vercel.app", "http://localhost:5173/"],
+    origin: ["https://tarek-jarvis-client.vercel.app", "http://localhost:5173"],
     credentials: true
   }
 ));
